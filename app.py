12f94d160d9813cbca28ee7c9d6bde582e86da2f
Georgia Lowe <georgiaelisha07@gmail.com>
2025-04-03 20:21:55 +0100

    commit

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['GET', 'POST'])
+def index_post():
+    fname = request.form.get('fname')
+    sname = request.form.get('sname')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    addrline = request.form.get('addrline')
+    towncity = request.form.get('towncity')
+    county = request.form.get('county')
+    postcode = request.form.get('postcode')
+
+    return render_template('index.html')
 
 
 
