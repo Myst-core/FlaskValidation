f27bbca3a93283cc32fb35b2d99e5f522468c2fa
Georgia Lowe <georgiaelisha07@gmail.com>
2025-04-03 21:47:35 +0100

    presence validation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'Secret'
 
 @app.route('/')
 def index():
@@ -17,9 +18,27 @@ def index_post():
     county = request.form.get('county')
     postcode = request.form.get('postcode')
 
+    # check if any input is empty
+    if fname == '' or sname == '' or email == '' or phone == '' or addrline == '' or towncity == '' or county == '' or postcode == '':
+        flash('All inputs are required', 'error')
+    
+    # check fname is between 3-30 char
+    if len(fname) < 3 or len(fname) > 30:
+        flash('Firstname must be 3-30 characters long', 'error')
+
+    # check sname is between 3-50 char
+    if len(sname) < 3 or len(sname) > 50:
+        flash('Surname must be 3-50 characters long', 'error')
+    
+    # check email is less than 60 char
+    if len(email) > 60:
+        flash('Email must be less than 60 characters long', 'error')
+    
+    if len(phone) != 11:
+        flash()
+
+
     return render_template('index.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
